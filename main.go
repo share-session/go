e3885136d7136d8b2f77fc42f8fa5f7f0fca5c38
Jack Allen <jackallensemail+git@gmail.com>
2023-05-25 15:19:51 +0100

    cookies

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package sharesession
 import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
+	"net/http"
 )
 
 func Create(session *Session) (*Session, error) {
@@ -22,6 +23,68 @@ func Create(session *Session) (*Session, error) {
 	}
 
 	return &response, nil
+}
+
+func New(session *Session) (*Session, error) {
+	client := resty.New().SetBaseURL("https://functions.share-session.com/api")
+
+	var response Session
+
+	_, err := client.
+		R().
+		EnableTrace().
+		SetBody(session).
+		SetResult(&response).
+		Post("s")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+func (session *Session) SetCookie(name string, value Cookie) *Session {
+	if session.Cookies == nil {
+		session.Cookies = map[string]Cookie{}
+	}
+
+	session.Cookies[name] = value
+
+	return session
+}
+
+func (session *Session) SetCookies(cookies map[string]Cookie) *Session {
+	session.Cookies = cookies
+
+	return session
+}
+
+func (session *Session) SetHttpCookie(cookie *http.Cookie) *Session {
+	session.Cookies[cookie.Name] = Cookie{
+		Name:     cookie.Name,
+		Value:    cookie.Value,
+		Domain:   cookie.Domain,
+		Path:     cookie.Path,
+		Secure:   cookie.Secure,
+		HttpOnly: cookie.HttpOnly,
+	}
+
+	return session
+}
+
+func (session *Session) SetHttpCookies(cookies []*http.Cookie) *Session {
+	for _, cookie := range cookies {
+		session.SetHttpCookie(cookie)
+	}
+
+	return session
+}
+
+func (session *Session) SetHTML(html string) *Session {
+	session.HTML = html
+
+	return session
 }
 
 func Get(id string) (*Session, error) {
--- types.go
+++ types.go
@@ -1,13 +1,21 @@
 package sharesession
 
-type Cookies map[string]interface{}
-type LocalStorage map[string]interface{}
-type SessionStorage map[string]interface{}
+type Cookie struct {
+	Name     string `json:"name"`
+	Value    string `json:"value"`
+	Path     string `json:"path"`
+	Domain   string `json:"domain"`
+	Secure   bool   `json:"secure"`
+	HttpOnly bool   `json:"httpOnly"`
+}
+
+type LocalStorage string
+type SessionStorage string
 
 type Session struct {
-	Id             string          `json:"id"`
-	Cookies        *Cookies        `json:"cookies"`
-	LocalStorage   *LocalStorage   `json:"localStorage"`
-	SessionStorage *SessionStorage `json:"sessionStorage"`
-	HTML           string          `json:"html"`
+	Id             string                    `json:"id"`
+	Cookies        map[string]Cookie         `json:"cookies"`
+	LocalStorage   map[string]LocalStorage   `json:"localStorage"`
+	SessionStorage map[string]SessionStorage `json:"sessionStorage"`
+	HTML           string                    `json:"html"`
 }
